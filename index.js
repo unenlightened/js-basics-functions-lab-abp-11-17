7a15ce00b540d9a90377c3b410086635dae56993
Chely Ho <adept_inept@yahoo.com>
2017-11-06 06:38:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const blockVertHQ = 42;
+
+function distanceFromHqInBlocks(blockVertStart){
+  return Math.abs(blockVertHQ - blockVertStart);
+}
+
+function distanceFromHqInFeet(blockVertStart){
+  return distanceFromHqInBlocks(blockVertStart) * 264;
+}
+
+function distanceTravelledInFeet (blockVertStart, blockVertEnd){
+ return Math.abs(blockVertStart - blockVertEnd) * 264;
+}
+
+function calculatesFarePrice(blockVertStart, blockVertEnd){
+  const distance = distanceTravelledInFeet (blockVertStart, blockVertEnd);
+
+  switch (true) {
+    case distance > 2500:
+      return 'cannot travel that far';
+    case distance > 2000:
+      return 25;
+    case distance > 400:
+      return distance * 0.02;       //first 400 ft are free, but it only passes when you calculate full distance
+    default:
+      return 0;
+  }
+}
